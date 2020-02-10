bf4a0e30ada66c62b30b47b7b061f137190685d2
kimjl <jamesleekim90@gmail.com>
2020-02-10 18:58:09 -0500

    Color Boxes

---
+++ state-exercises/color-boxes/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Box from './Box';
+import BoxContainer from './BoxContainer';
+import './App.css';
+
+class App extends Component {
+	render() {
+		return (
+			<div className="App">
+				<BoxContainer />
+			</div>
+		);
+	}
+}
+
+export default App;
