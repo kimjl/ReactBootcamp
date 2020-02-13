6a559e5fd609f0431253d0577944a5f533baac4a
kimjl <jamesleekim90@gmail.com>
2020-02-12 19:18:25 -0500

    box exercise

---
+++ boxlist-exercise/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import BoxList from './BoxList';
+
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+	render() {
+		return (
+			<div className="App">
+				<BoxList />
+			</div>
+		);
+	}
+}
+
+export default App;
